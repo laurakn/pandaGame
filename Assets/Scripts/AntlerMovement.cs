23ba199dbc01d5e2992c7d52a746965a69833121
laurakn <laura.niss@gmail.com>
2019-12-28 12:43:56 -0500

    Work in progress

--- Assets/Scripts/AntlerMovement.cs
+++ Assets/Scripts/AntlerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AntlerMovement : MonoBehaviour {
     public float speed;
     public LayerMask wallLayer;
