2f65029d1e2dca1bdb89efc550ca1148ad907317
laurakn <laura.niss@gmail.com>
2019-12-28 10:13:10 -0500

    Changes rigidbody to character controller with raycasting for collisions

---
+++ Assets/Scriptes/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject objectToFollow;
+
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    void Start ()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - objectToFollow.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate ()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        this.transform.position = objectToFollow.transform.position;
+    }
+
+}
---
+++ Assets/Scriptes/PlayerInput.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof (Player))]
+public class PlayerInput : MonoBehaviour {
+
+	Player player;
+  Animator animator;
+
+	void Start () {
+		player = GetComponent<Player> ();
+    animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		player.SetDirectionalInput (directionalInput);
+
+    bool moving = directionalInput.x != 0;
+
+    animator.SetBool("grounded", player.grounded);
+    animator.SetBool("moving", moving);
+
+    if ((directionalInput.x < 0) != player.facingLeft && moving) {
+      player.turn();
+    }
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			player.OnJumpInputDown ();
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			player.OnJumpInputUp ();
+		}
+	}
+}
