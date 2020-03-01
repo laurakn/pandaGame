915be108a80a9dcacdd8039d42116fc2e2814121
laurakn <laura.niss@gmail.com>
2020-02-29 19:02:16 -0500

    snow puff in glacier scene

---
+++ Assets/Scripts/Scenes/Glacier/ImpactSnow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSnow : MonoBehaviour {
+
+    public GameObject snowObject;
+    // Start is called before the first frame update
+    void Start() { 
+    }
+
+    void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.collider.CompareTag ("Player")) {
+            Vector2 position = collision.GetContact(0).point; 
+            snowPuff(position);
+        }
+    }
+    void snowPuff (Vector2 position) {
+    GameObject clone = Instantiate(snowObject, new Vector3(position.x, position.y, 1), Quaternion.identity);
+    clone.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Scenes/Glacier/SelfDestruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+    public int timeToDestruction = 5;
+
+    
+    void Awake() {
+        Destroy(gameObject, timeToDestruction);
+    }
+}
